0e52d586a1803fb11572ab4cd9ec3a6958dacd5a
Rangareddy <rangareddyjava9@gmal.com>
2019-10-02 06:55:58 +0530

    backend

---
+++ springboot2-jpa-crud-example/src/main/java/com/ranga/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.ranga.repository;
+
+import com.ranga.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
